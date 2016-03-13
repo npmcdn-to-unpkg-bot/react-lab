4028be5a14f3091ce0bacd976f8bcf0752bb9655
zhangshibiao <zhangshibiao@baidu.com>
2016-03-13 21:19:23 +0800

    dnd and redux

--- client/components/editor/component-list.jsx
+++ client/components/editor/component-list.jsx
@@ -1,5 +1,82 @@
 import React from 'react';
-export default class Componentlist extends  React.Component {
+import {PropTypes} from 'react';
+import { DragSource, DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import Comwraper from './com-wraper.jsx';
+class Card extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        let text = this.props.text;
+        return (
+            <div><div data-config={JSON.stringify({name:'card-1'})}>卡片</div></div>
+        );
+    }
+}
+
+const ItemTypes = {
+    CARD: 'card',
+    TUPI: 'tupian'
+};
+/**
+ * Implements the drag source contract.
+ */
+const cardSource = {
+    beginDrag(props) {
+        return {
+            text: props.text
+        }
+    },
+    endDrag(props, monitor) {
+        const item = monitor.getItem();
+        const dropResult = monitor.getDropResult();
+        //debugger;
+        props.onToAdd(JSON.stringify({name:'card-1'}));
+        if (dropResult) {
+            window.alert( // eslint-disable-line no-alert
+                `You dropped ${item.text} into ${dropResult.name}!`
+            );
+        }
+    }
+};
+/**
+ * Specifies the props to inject into your component.
+ */
+function collect(connect, monitor) {
+    return {
+        connectDragSource: connect.dragSource(),
+        isDragging: monitor.isDragging()
+    };
+}
+const propTypes = {
+    text: PropTypes.string.isRequired,
+    // Injected by React DnD:
+    isDragging: PropTypes.bool.isRequired,
+    connectDragSource: PropTypes.func.isRequired
+};
+class DragWraperCom extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const { isDragging, connectDragSource } = this.props;
+        const { text } = this.props;
+        let props = this.props;
+        return connectDragSource(
+            <div>
+                <Card {...props}/>
+                I am a draggable card number {text}
+                {isDragging && ' (and I am being dragged now)'}
+            </div>
+        );
+    }
+}
+
+// add  drag  wraper to component
+var DragcomwraperSource = DragSource(ItemTypes.CARD, cardSource, collect)(DragWraperCom);
+
+class Componentlist extends  React.Component {
     constructor(props) {
         super(props);
     }
@@ -15,16 +92,32 @@ export default class Componentlist extends  React.Component {
     }
     render() {
         let that = this;
+        console.log('Componentlist props', this.props);
+        let props = this.props;
         return (
             <div className="c-list">
+                <DragcomwraperSource text={'----'} {...props}/>
                 <ul>
-                    <li onClick={this.onClickHandler.bind(that)} data-config={JSON.stringify({name:'card-1'})}>组件1</li>
-                    <li onClick={this.onClickHandler.bind(that)} data-config={JSON.stringify({name:'card-2'})}>组件2</li>
-                    <li onClick={this.onClickHandler.bind(that)} data-config={JSON.stringify({name:'card-3'})}>组件3</li>
-                    <li onClick={this.onClickHandler.bind(that)} data-config={JSON.stringify({name:'card-4'})}>组件4</li>
-                    <li onClick={this.onClickHandler.bind(that)} data-config={JSON.stringify({name:'card-5'})}>组件5</li>
+                    <li onClick={this.onClickHandler.bind(this)}  data-config={JSON.stringify({name:'card-1'})}>组件1</li>
                 </ul>
             </div>
         );
     }
 }
+class ComponentlistDragContext extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        let props = this.props;
+        return (
+            <div className="c-list">
+               <Componentlist {...props}/>
+                <div style={{ overflow: 'hidden', clear: 'both' }}>
+                    <Comwraper />
+                </div>
+            </div>
+        );
+    }
+}
+export default DragDropContext(HTML5Backend)(ComponentlistDragContext)
